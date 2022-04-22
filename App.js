9982e656f6dc21f596606148154364b8c28267e0
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-22 23:25:34 +0300

    Placeholder for dialogConstructor

--- App.js
+++ App.js
@@ -208,6 +208,7 @@ async function start() {
 Server on: ${PORT}
 http://localhost:${PORT}/
 http://localhost:${PORT}/auth.html
+http://localhost:${PORT}/dialogConstructor.html
 figma:
 https://www.figma.com/file/e6CPWViwJTFCZxUp18VT1u/AI-constructor?node-id=255%3A270
 -------------------------------
