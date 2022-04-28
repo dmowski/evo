f389d01f2634fefdc9155e901f2fb1df0ff03043
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-28 13:20:13 +0300

    TMP

--- static/dialogConstructorDev/src/components/Node/CustomNodeWidget.tsx
+++ static/dialogConstructorDev/src/components/Node/CustomNodeWidget.tsx
@@ -1,106 +1,109 @@
-import * as React from 'react';
-import { CustomNodeModel } from './CustomNodeModel';
+import * as React from "react";
+import { CustomNodeModel } from "./CustomNodeModel";
 import {
-    DiagramEngine,
-    NodeModel,
-    PortWidget,
-} from '@projectstorm/react-diagrams';
+  DiagramEngine,
+  NodeModel,
+  PortWidget,
+} from "@projectstorm/react-diagrams";
 import {
-    DeleteNodeButton,
-    GraphNode,
-    GraphNodeErrorMessage,
-    GraphNodeHeader,
-    NodeViews,
-    Port,
-} from './style';
+  DeleteNodeButton,
+  GraphNode,
+  GraphNodeErrorMessage,
+  GraphNodeHeader,
+  NodeViews,
+  Port,
+} from "./style";
 
 export interface CustomNodeWidgetProps {
-    node: CustomNodeModel;
-    engine: DiagramEngine;
-    type: string;
-    name: string;
-    content: string;
-    isIn: boolean;
-    isSelected: boolean;
+  node: CustomNodeModel;
+  engine: DiagramEngine;
+  type: string;
+  name: string;
+  content: string;
+  isIn: boolean;
+  isSelected: boolean;
 }
 
 export const CustomNodeWidget = ({
-                                     engine,
-                                     node,
-                                     type,
-                                     name,
-                                     isIn,
-                                     isSelected,
-                                     content,
-                                 }: CustomNodeWidgetProps) => {
-    const [nameValue, setName] = React.useState(name);
-    const [errorName, setErrorName] = React.useState('');
-    const setNewNodeName = (name: string) => {
-        const newNameResultError = node.setNewNodeName(name);
-        if (newNameResultError) {
-            setErrorName(newNameResultError);
-        } else {
-            setErrorName('');
-        }
-        setName(name);
-    };
-    const setNewNodeContent = (nodeContent: string) => {
-        node.setNewNodeContent(nodeContent);
-    };
+  engine,
+  node,
+  type,
+  name,
+  isIn,
+  isSelected,
+  content,
+}: CustomNodeWidgetProps) => {
+  const [nameValue, setName] = React.useState(name);
+  const [errorName, setErrorName] = React.useState("");
+  const setNewNodeName = (name: string) => {
+    const newNameResultError = node.setNewNodeName(name);
+    if (newNameResultError) {
+      setErrorName(newNameResultError);
+    } else {
+      setErrorName("");
+    }
+    setName(name);
+  };
+  const setNewNodeContent = (nodeContent: string) => {
+    node.setNewNodeContent(nodeContent);
+  };
 
-    const activeColor = isIn ? '#FF7A00' : '#1A1A4E';
-    return (
-        <GraphNode
-            activeColor={activeColor}
-            isError={!!errorName}
-            className={`custom-node ${isSelected ? 'is-selected' : ''}`}
-        >
-            <GraphNodeHeader activeColor={activeColor}>
-                <p>{type}</p>
-                <input
-                    value={nameValue}
-                    onChange={(e) => setNewNodeName(e.target.value)}
-                />
-            </GraphNodeHeader>
-            {errorName && (
-                <GraphNodeErrorMessage>
-                    Error with name: {errorName}
-                </GraphNodeErrorMessage>
-            )}
+  const activeColor = isIn ? "#FF7A00" : "#1A1A4E";
+  return (
+    <GraphNode
+      activeColor={activeColor}
+      isError={!!errorName}
+      className={`custom-node ${isSelected ? "is-selected" : ""}`}
+    >
+      <GraphNodeHeader activeColor={activeColor}>
+        <p>{type}</p>
+        <input
+          value={nameValue}
+          onChange={(e) => setNewNodeName(e.target.value)}
+        />
+      </GraphNodeHeader>
+      {errorName && (
+        <GraphNodeErrorMessage>
+          Error with name: {errorName}
+        </GraphNodeErrorMessage>
+      )}
 
-            <textarea defaultValue={content} onChange={(e) => setNewNodeContent(e.target.value)}></textarea>
-            <PortWidget
-                style={{
-                    top: '-10px',
-                    left: 120,
-                    position: 'absolute',
-                }}
-                port={node.getPort(isIn ? 'in' : 'out')}
-                engine={engine}
-            >
-                <Port/>
-            </PortWidget>
-            <DeleteNodeButton
-                style={{}}
-                onClick={() => {
-                    const confirmResult = confirm('Delete this element?');
-                    if (!confirmResult) {
-                        return;
-                    }
-                    engine
-                        .getModel()
-                        .getSelectedEntities()
-                        .forEach((item) => {
-                            if (item instanceof NodeModel) {
-                                engine.getModel().removeNode(item);
-                                engine.repaintCanvas();
-                            }
-                        });
-                }}
-            >
-                🗑
-            </DeleteNodeButton>
-            <NodeViews>9</NodeViews>
-        </GraphNode>
-    );
+      <textarea
+        defaultValue={content}
+        onChange={(e) => setNewNodeContent(e.target.value)}
+      ></textarea>
+      <PortWidget
+        style={{
+          top: "-10px",
+          left: 120,
+          position: "absolute",
+        }}
+        port={node.getPort(isIn ? "in" : "out")}
+        engine={engine}
+      >
+        <Port />
+      </PortWidget>
+      <DeleteNodeButton
+        style={{}}
+        onClick={() => {
+          const confirmResult = confirm("Delete this element?");
+          if (!confirmResult) {
+            return;
+          }
+          engine
+            .getModel()
+            .getSelectedEntities()
+            .forEach((item) => {
+              if (item instanceof NodeModel) {
+                engine.getModel().removeNode(item);
+                engine.repaintCanvas();
+              }
+            });
+        }}
+      >
+        🗑
+      </DeleteNodeButton>
+      <NodeViews>9</NodeViews>
+    </GraphNode>
+  );
 };
