139ac3cbeed9d19360f2c2af716aea5a34212efd
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-30 00:30:40 +0300

    Update backend

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ app.get("/api/v1/ping", (req, res) => {
   res.json({ message: "ok" });
 });
 
+const listOfGraphs = [];
+
 /*
 Отправка диалогового скрипта:
 
@@ -28,14 +30,18 @@ POST api/v1/dialo_graph.update
             "node_name": "greet",
             "node_content": "hey\nhey there\nhello\nhi\ngood morning",
             "node_links": ["greet"],
-            "node_views": 0
+            "node_views": 0,
+            "position_x": 10,
+            "position_y": 10
         },
         {
             "node_type": "skill",
             "node_name": "greet",
             "node_content": "hey\nhey there\nhello\nhi\ngood morning",
             "node_links": [],
-            "node_views": 0
+            "node_views": 0,
+            "position_x": 10,
+            "position_y": 10
         }
     ]
 }
@@ -51,12 +57,21 @@ POST api/v1/dialo_graph.update
         node_content - реплики пользователя или бота, разделённые переводом каретки.
         node_links - ссылки из данной ноды на другие ноды. (Это те самые стрелочки). Ссылок может быть от 0 до сколько угодно большого количества. Главное условие - типы последующих нод должны быть иными.
         node_views - "Срабатывания" данной ноды при общении бота с пользователем. Пока оставляем как 0.
+        position_x
+        position_y
 
 */
 
 const validateGraphNode = (graphNode) => {
-  const { node_type, node_name, node_content, node_links, node_views } =
-    graphNode;
+  const {
+    node_type,
+    node_name,
+    node_content,
+    node_links,
+    node_views,
+    position_x,
+    position_y,
+  } = graphNode;
 
   console.log(graphNode);
 
@@ -73,13 +88,22 @@ const validateGraphNode = (graphNode) => {
   if (!node_links || !Array.isArray(node_links)) {
     return "'node_links' should be an array of scrings";
   }
+  if (typeof position_x !== "number") {
+    return "'position_x' should be a number";
+  }
+  if (typeof position_y !== "number") {
+    return "'position_y' should be a number";
+  }
+
   if (typeof node_views !== "number") {
     return "'node_views' should be a number";
   }
 };
 
+// UPDATE GRAPH
 app.post("/api/v1/dialo_graph.update", (req, res) => {
   const { id, title, graph } = req.body;
+
   if (!id) {
     return res.json({ error: "'id' is missing. Root level" });
   }
@@ -118,37 +142,106 @@ app.post("/api/v1/dialo_graph.update", (req, res) => {
     }
     indexOfNode++;
   }
+
   console.log("api/v1/dialo_graph.update");
   console.log("Validation passed");
+
+  const graphInList = listOfGraphs.find((graphNode) => {
+    return graphNode.id == id;
+  });
+
+  if (!graphInList) {
+    res.json({ error: "Graph not found" });
+    return;
+  }
+
+  graphInList.title = title;
+  graphInList.graph = graph;
+
+  res.json({ message: "ok" });
+});
+
+// Add GRAPH
+app.post("/api/v1/dialo_graph.add", (req, res) => {
+  const { title, graph } = req.body;
+
+  if (!title) {
+    return res.json({ error: "'title' is missing. Root level" });
+  }
+
+  if (typeof title !== "string") {
+    return res.json({ error: "'title' should be a string" });
+  }
+
+  if (!graph) {
+    return res.json({ error: "'graph' property is missing. Root level" });
+  }
+
+  if (!Array.isArray(graph)) {
+    return res.json({ error: "'graph' should be an array. Root level" });
+  }
+  let indexOfNode = 0;
+
+  for (const graphNode of graph) {
+    const validateGraphNodeResult = validateGraphNode(graphNode);
+    if (validateGraphNodeResult) {
+      return res.json({
+        error: "graph node failed. ",
+        message: validateGraphNodeResult,
+        node: graphNode,
+        indexOfNode: indexOfNode,
+        body: req.body,
+      });
+    }
+    indexOfNode++;
+  }
+
+  console.log("api/v1/dialo_graph.add");
+  console.log("Validation passed");
+
+  listOfGraphs.push({
+    id: Date.now(),
+    title,
+    graph,
+  });
 
   res.json({ message: "ok" });
 });
 
 /*
-
-
-
 Запрос диалогового скрипта для просмотра / редактирования
 
 GET api/v1/dialo_graph.get
 {"graph_id": 1}
 */
-
 app.get("/api/v1/dialo_graph.get", (req, res) => {
   const graphId = req.query.graph_id;
   if (!graphId) {
     return res.json({
       message:
-        "graphId param misssing. try this: http://localhost:62544/api/v1/dialo_graph.get?graph_id=2",
+        "graph_id param misssing. try this: http://localhost:62544/api/v1/dialo_graph.get?graph_id=2",
       error: true,
     });
   }
 
   console.log("api/v1/dialo_graph.get");
+  console.log(listOfGraphs);
   console.log("graphId", graphId);
   console.log("------------------");
+  const graph = listOfGraphs.find((graphNode) => {
+    return graphNode.id == graphId;
+  });
 
-  res.json({
+  if (graph) {
+    res.json(graph);
+    return;
+  }
+  return res.json({
+    message: "Graph not founded",
+    error: true,
+  });
+
+  /*res.json({
     id: 1,
     title: "Заголовок диалогового графа",
     graph: [
@@ -158,6 +251,8 @@ app.get("/api/v1/dialo_graph.get", (req, res) => {
         node_content: "hey\nhey there\nhello\nhi\ngood morning",
         node_links: ["greet"],
         node_views: 0,
+        position_x: 100,
+        position_y: 100
       },
       {
         node_type: "skill",
@@ -165,22 +260,31 @@ app.get("/api/v1/dialo_graph.get", (req, res) => {
         node_content: "hey\nhey there\nhello\nhi\ngood morning",
         node_links: [],
         node_views: 0,
+        position_x: 300,
+        position_y: 300
       },
     ],
-  });
+  });*/
 });
+
 /*
-
-
-
-
 Список имеющихся на сервере графов
 GET api/v1/dialo_graph.list
 Ответ:
-
 */
 app.get("/api/v1/dialo_graph.list", function (req, res) {
+  const list = listOfGraphs.map((graph) => {
+    return {
+      id: graph.id,
+      title: graph.title,
+    };
+  });
+
   res.json({
+    graph_list: list,
+  });
+
+  /*res.json({
     graph_list: [
       {
         id: 1,
@@ -191,7 +295,7 @@ app.get("/api/v1/dialo_graph.list", function (req, res) {
         title: "Диалоговый скрипт 2",
       },
     ],
-  });
+  });*/
 });
 
 app.use(express.static(path.join(__dirname, "static")));
