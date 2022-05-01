375066180164f0250f6cffa02e3275eb10ca92e8
dllsc <92045376+dllsc@users.noreply.github.com>
2022-05-02 02:11:25 +0300

    prettiert fix

--- static/dialogConstructorDev/src/components/MainLayout.tsx
+++ static/dialogConstructorDev/src/components/MainLayout.tsx
@@ -12,13 +12,15 @@ import {
   NodeControlPanel,
   ZoomControlButton,
 } from "./style";
-import { BackendGraphNode, dataForPost, receivedData } from './utils/dataProcessing';
+import {
+  BackendGraphNode,
+  dataForPost,
+  receivedData,
+} from "./utils/dataProcessing";
 
 export interface MainLayoutProps {
   app: Application;
 }
-
-
 
 export interface BackendGraph {
   id: string;
@@ -32,8 +34,8 @@ export interface BackendShortGraph {
 }
 
 const baseUrl = location.host.startsWith("localhost:34567")
-    ? "http://localhost:62544/"
-    : location.origin + "/";
+  ? "http://localhost:62544/"
+  : location.origin + "/";
 
 const getListOfGraphs = async (): Promise<BackendShortGraph[]> => {
   const req = await fetch(baseUrl + "api/v1/dialo_graph.list");
@@ -64,7 +66,7 @@ const addNewGraph = async (graphData: BackendGraph): Promise<BackendGraph> => {
 };
 
 const updateGraphInBD = async (
-    graphData: BackendGraph
+  graphData: BackendGraph
 ): Promise<BackendGraph> => {
   const response = await fetch(baseUrl + "api/v1/dialo_graph.update", {
     method: "POST",
@@ -168,7 +170,7 @@ export const MainLayout = ({ app }: MainLayoutProps) => {
 
   const newGraph = async () => {
     const confirmMessage =
-        "Создать новый диалог? Введенные данные будут утеряны";
+      "Создать новый диалог? Введенные данные будут утеряны";
 
     if (selectGraph && !window.confirm(confirmMessage)) {
       return;
@@ -191,103 +193,103 @@ export const MainLayout = ({ app }: MainLayoutProps) => {
   };
 
   return (
-      <div>
-        <div
-            style={{
-              height: "100vh",
-            }}
-            onDrop={dropHandler}
-            onDragOver={(event) => {
-              event.preventDefault();
-            }}
-        >
-          {selectGraph && (
-              <GraphCanvas
-                  color="rgb(222, 222, 222)"
-                  background="rgb(233, 233, 233)"
-              >
-                <CanvasWidget engine={app.diagramEngine} />
-              </GraphCanvas>
-          )}
-        </div>
-
-        <DialogConstructorHeader>
-          {graphList.length > 0 && (
-              <select
-                  name="dialogs"
-                  value={selectGraph?.id}
-                  onChange={(e) => {
-                    changeGraph(e.target.value);
-                  }}
-              >
-                <option value="0"></option>
-                {graphList.map((graphInfo, key) => {
-                  return (
-                      <option value={graphInfo.id} key={graphInfo.id}>
-                        {graphInfo.title}
-                      </option>
-                  );
-                })}
-              </select>
-          )}
-
-          <AddDialogButton onClick={newGraph}>
-            Добавить новый диалог
-          </AddDialogButton>
-
-          {selectGraph && (
-              <AddDialogButton onClick={saveGraph}>Сохранить граф</AddDialogButton>
-          )}
-        </DialogConstructorHeader>
-
+    <div>
+      <div
+        style={{
+          height: "100vh",
+        }}
+        onDrop={dropHandler}
+        onDragOver={(event) => {
+          event.preventDefault();
+        }}
+      >
         {selectGraph && (
-            <>
-              <NodeControlPanel>
-                <NodeControlElement
-                    color="#1A1A4E"
-                    draggable={true}
-                    onDragStart={(event) => {
-                      event.dataTransfer.setData("storm-diagram-node-type", "intent");
-                    }}
-                    className="tray-item"
-                >
-                  Intent
-                </NodeControlElement>
-
-                <NodeControlElement
-                    color="#FF7A00"
-                    draggable={true}
-                    onDragStart={(event) => {
-                      event.dataTransfer.setData("storm-diagram-node-type", "skill");
-                    }}
-                    className="tray-item"
-                >
-                  Skill
-                </NodeControlElement>
-              </NodeControlPanel>
-
-              <ZoomControlButton
-                  onClick={() => {
-                    app.diagramEngine.zoomToFitSelectedNodes({ margin: 100 });
-                  }}
-              >
-                Zoom to fit
-              </ZoomControlButton>
-
-              <DeleteControlButton
-                  onClick={() => {
-                    const confirm = window.confirm(
-                        "Are you sure you want to delete Dialog?"
-                    );
-                    if (confirm) {
-                      console.log("Send request");
-                    }
-                  }}
-              >
-                🗑
-              </DeleteControlButton>
-            </>
+          <GraphCanvas
+            color="rgb(222, 222, 222)"
+            background="rgb(233, 233, 233)"
+          >
+            <CanvasWidget engine={app.diagramEngine} />
+          </GraphCanvas>
         )}
       </div>
+
+      <DialogConstructorHeader>
+        {graphList.length > 0 && (
+          <select
+            name="dialogs"
+            value={selectGraph?.id}
+            onChange={(e) => {
+              changeGraph(e.target.value);
+            }}
+          >
+            <option value="0"></option>
+            {graphList.map((graphInfo, key) => {
+              return (
+                <option value={graphInfo.id} key={graphInfo.id}>
+                  {graphInfo.title}
+                </option>
+              );
+            })}
+          </select>
+        )}
+
+        <AddDialogButton onClick={newGraph}>
+          Добавить новый диалог
+        </AddDialogButton>
+
+        {selectGraph && (
+          <AddDialogButton onClick={saveGraph}>Сохранить граф</AddDialogButton>
+        )}
+      </DialogConstructorHeader>
+
+      {selectGraph && (
+        <>
+          <NodeControlPanel>
+            <NodeControlElement
+              color="#1A1A4E"
+              draggable={true}
+              onDragStart={(event) => {
+                event.dataTransfer.setData("storm-diagram-node-type", "intent");
+              }}
+              className="tray-item"
+            >
+              Intent
+            </NodeControlElement>
+
+            <NodeControlElement
+              color="#FF7A00"
+              draggable={true}
+              onDragStart={(event) => {
+                event.dataTransfer.setData("storm-diagram-node-type", "skill");
+              }}
+              className="tray-item"
+            >
+              Skill
+            </NodeControlElement>
+          </NodeControlPanel>
+
+          <ZoomControlButton
+            onClick={() => {
+              app.diagramEngine.zoomToFitSelectedNodes({ margin: 100 });
+            }}
+          >
+            Zoom to fit
+          </ZoomControlButton>
+
+          <DeleteControlButton
+            onClick={() => {
+              const confirm = window.confirm(
+                "Are you sure you want to delete Dialog?"
+              );
+              if (confirm) {
+                console.log("Send request");
+              }
+            }}
+          >
+            🗑
+          </DeleteControlButton>
+        </>
+      )}
+    </div>
   );
 };
