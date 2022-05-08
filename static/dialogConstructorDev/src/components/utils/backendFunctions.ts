72fadbd2e1b2bdf07095581cffff2a4c5dfaaa5a
dllsc <92045376+dllsc@users.noreply.github.com>
2022-05-08 23:42:37 +0300

    Remove Links (#36)
    * remove links
    
    * prettier fix

--- static/dialogConstructorDev/src/components/utils/backendFunctions.ts
+++ static/dialogConstructorDev/src/components/utils/backendFunctions.ts
@@ -1,49 +1,49 @@
-import { BackendGraph, BackendShortGraph } from '../MainLayout';
+import { BackendGraph, BackendShortGraph } from "../MainLayout";
 
 const baseUrl = location.host.startsWith("localhost:34567")
-    ? "http://localhost:62544/"
-    : location.origin + "/";
+  ? "http://localhost:62544/"
+  : location.origin + "/";
 
 export const getListOfGraphs = async (): Promise<BackendShortGraph[]> => {
-    const req = await fetch(baseUrl + "api/v1/dialo_graph.list");
-    const listOfNodes = await req.json();
-    return listOfNodes?.graph_list || [];
+  const req = await fetch(baseUrl + "api/v1/dialo_graph.list");
+  const listOfNodes = await req.json();
+  return listOfNodes?.graph_list || [];
 };
 
 export const getOneGraph = async (id: string): Promise<BackendGraph> => {
-    const req = await fetch(baseUrl + "api/v1/dialo_graph.get?graph_id=" + id);
-    const graphData = await req.json();
-    return graphData;
+  const req = await fetch(baseUrl + "api/v1/dialo_graph.get?graph_id=" + id);
+  const graphData = await req.json();
+  return graphData;
 };
 
 export const addNewGraph = async (graphData: BackendGraph): Promise<BackendGraph> => {
-    const response = await fetch(baseUrl + "api/v1/dialo_graph.add", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(graphData),
-    });
-    const result = await response.json();
-    console.log("Saving result", result);
+  const response = await fetch(baseUrl + "api/v1/dialo_graph.add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(graphData),
+  });
+  const result = await response.json();
+  console.log("Saving result", result);
 
-    if (result.error) {
-        return result.error + result.message;
-    }
+  if (result.error) {
+    return result.error + result.message;
+  }
 };
 
 export const updateGraphInBD = async (graphData: BackendGraph): Promise<BackendGraph> => {
-    const response = await fetch(baseUrl + "api/v1/dialo_graph.update", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(graphData),
-    });
-    const result = await response.json();
-    console.log("Saving result", result);
+  const response = await fetch(baseUrl + "api/v1/dialo_graph.update", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(graphData),
+  });
+  const result = await response.json();
+  console.log("Saving result", result);
 
-    if (result.error) {
-        return result.error + result.message;
-    }
+  if (result.error) {
+    return result.error + result.message;
+  }
 };
