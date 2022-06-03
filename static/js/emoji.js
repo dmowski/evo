0d8abbff73b63ed0fd24025a6b23d19977fe6ad1
dllsc <92045376+dllsc@users.noreply.github.com>
2022-06-03 17:52:03 +0300

    update emoji

--- static/js/emoji.js
+++ static/js/emoji.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const emoji = document.querySelector(".emoji-control__list");
+  const emoji = document.querySelector(".emoji-list");
   const emojiToggler = document.querySelector(".emoji-toggler");
 
   emojiToggler.addEventListener("click", () => {
